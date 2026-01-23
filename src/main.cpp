5057ead80e7684c9a1006aa5a38b795e68f3fbb1
myanwc <anton.kostyrev@yandex.ru>
2026-01-23 21:51:42 +0300

    change example

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@
 
 int main() {
     proc_scan::ProcessScanner proc_scanner;
+    proc_scanner.FindHidenProcesses();
+    //proc_scanner.CreateSnapshot();
+    //proc_scanner.PrintLastFullSnapshot(std::cout);
+
+    //if (auto proc = proc_scanner.GetProcessInfo("Telegram.exe")) {
+        //std::cout << "Founded: \n";
+        //proc->Print(std::cout);
     //}
 
 
