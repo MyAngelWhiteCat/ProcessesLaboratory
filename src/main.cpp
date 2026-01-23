fd677763caacb51384d94dedbcb8c1d6ee173fd9
myanwc <anton.kostyrev@yandex.ru>
2026-01-23 21:51:31 +0300

    some refs

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,5 @@ int main() {
     proc_scan::ProcessScanner proc_scanner;
     //}
 
-    proc_scanner.CreateSnapshot();
-    proc_scanner.PrintLastFullSnapshot(std::cout);
 
-    if (auto proc = proc_scanner.GetProcessInfo("Telegram.exe")) {
-        std::cout << "Founded: \n";
-        proc->Print(std::cout);
-    }
 }
