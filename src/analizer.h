3b5cdec85f5895e73e4510e47fbae3922fd9a648
myanwc <anton.kostyrev@yandex.ru>
2026-02-05 15:06:04 +0300

    Little refactoring

--- src/analizer.h
+++ src/analizer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <memory>
 #include <optional>
 #include <unordered_map>
 #include <vector>
@@ -12,7 +13,6 @@ namespace proc_scan {
     namespace labaratory {
 
         using Clock = std::chrono::high_resolution_clock;
-        using ScanResult = std::unordered_map<proc_scan::domain::ScanMethod, domain::Snapshot>;
 
         class Analizer;
         using Analizers = std::unordered_map<domain::AnalizerType, std::unique_ptr<Analizer>>;
