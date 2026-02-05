db584dc3b5ed7236193b2c500c794460d129601d
myanwc <anton.kostyrev@yandex.ru>
2026-02-05 12:53:35 +0300

    There is just a dummy for compiling tests

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 
 int main() {
     proc_scan::ProcessScanner proc_scanner;
-    proc_scanner.FindHidenProcesses();
-    //proc_scanner.CreateSnapshot();
-    //proc_scanner.PrintLastFullSnapshot(std::cout);
-
-    //if (auto proc = proc_scanner.GetProcessInfo("Telegram.exe")) {
-        //std::cout << "Founded: \n";
-        //proc->Print(std::cout);
-    //}
+    try {
+        if (auto proc = proc_scanner.GetProcessInfo("timer.exe")) {
+            std::cout << "Founded: \n";
+            proc->Print(std::cout);
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 
 
 }
