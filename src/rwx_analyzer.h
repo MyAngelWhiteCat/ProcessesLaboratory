d39f4483d8a802d682c52cdac59ba5309794d9a3
myanwc <anton.kostyrev@yandex.ru>
2026-02-08 13:27:43 +0300

    Add includes

--- src/rwx_analyzer.h
+++ src/rwx_analyzer.h
@@ -7,10 +7,15 @@
 #include <string>
 
 #include <Windows.h>
+#include <string_view>
+#include <utility>
 
 namespace proc_scan {
 
     namespace labaratory {
+
+        const SIZE_T KB = 1024;
+        const SIZE_T MB = KB * 1024;
 
         class RWXAnalyzer : public Analyzer {
 
