ccff1eb6227e86170de4acf5f8246fca1f9f429c
myanwc <anton.kostyrev@yandex.ru>
2026-02-08 13:30:10 +0300

    Fix memory check algorithm

--- src/rwx_analyzer.cpp
+++ src/rwx_analyzer.cpp
@@ -20,7 +20,7 @@ namespace proc_scan {
 
             AnalyzeResult result;
             for (const auto& [pid, proc_info] : snapshot.pid_to_proc_info_) {
-                LOG_DEBUG("Process ["s + std::to_string(proc_info->GetPid())+ "] "s
+                LOG_DEBUG("Process ["s + std::to_string(proc_info->GetPid()) + "] "s
                     + std::string(proc_info->GetProcessName()));
 
                 HANDLE hProcess = proc_info->Open(PROCESS_QUERY_INFORMATION);
@@ -30,43 +30,69 @@ namespace proc_scan {
                 }
             }
             return result;
-            
         }
 
         std::string RWXAnalyzer::AnalyzeProcessMemory(HANDLE hProcess) {
             LOG_DEBUG("Start RWX analyze");
             std::string comments;
             MEMORY_BASIC_INFORMATION memory_info{ 0 };
-            SIZE_T address = NULL; 
+            SIZE_T address = NULL;
+
+            std::vector<SIZE_T> rwx_regions{ 0 };
+            std::vector<SIZE_T> rx_to_rw_regions{ 0 };
+            std::vector<SIZE_T> rw_to_rx_regions{ 0 };
             while (VirtualQueryEx(hProcess, (LPVOID)address, &memory_info, sizeof(memory_info))) {
-                //std::cout
-                //    <<   "AllocBase:---- " << memory_info.AllocationBase
-                //    << "\nAllocProtect:- " << memory_info.AllocationProtect
-                //    << "\nBaseAddress:-- " << memory_info.BaseAddress
-                //    << "\nPartitionId:-- " << memory_info.PartitionId
-                //    << "\nProtect:------ " << memory_info.Protect
-                //    << "\nRegionSize:--- " << memory_info.RegionSize
-                //    << "\nState:-------- " << memory_info.State
-                //    << "\nType:--------- " << memory_info.Type
-                //    << "\n";
+                address = (SIZE_T)memory_info.BaseAddress + memory_info.RegionSize;
+                if (address == 0) {
+                    throw std::runtime_error("Empty region");
+                }
+                if (memory_info.State != MEM_COMMIT) {
+                    continue;
+                }
 
                 if (memory_info.Protect == PAGE_EXECUTE_READWRITE) {
-                    comments += "RWX rights detected!\n";
+                    rwx_regions.back() += memory_info.RegionSize;
+                }
+                else if (rwx_regions.back()) {
+                    rwx_regions.push_back(0);
                 }
 
                 if (memory_info.AllocationProtect == PAGE_EXECUTE_READ
                     && memory_info.Protect == PAGE_READWRITE) {
-                    comments += "RX to RW swap detected!\n";
+                    rx_to_rw_regions.back() += memory_info.RegionSize;
+                }
+                else if (rx_to_rw_regions.back()) {
+                    rx_to_rw_regions.push_back(0);
                 }
 
                 if (memory_info.AllocationProtect == PAGE_READWRITE
                     && memory_info.Protect == PAGE_EXECUTE_READ) {
-                    comments += "RW to RX swap detected!\n";
+                    rw_to_rx_regions.back() += memory_info.RegionSize;
+                }
+                else if (rw_to_rx_regions.back()) {
+                    rw_to_rx_regions.push_back(0);
                 }
 
-                address += (SIZE_T)memory_info.BaseAddress + memory_info.RegionSize;
-                if (address == 0) {
-                    break;
+            }
+            rwx_regions.pop_back();
+            rx_to_rw_regions.pop_back();
+            rw_to_rx_regions.pop_back();
+            if (!rwx_regions.empty()) {
+                for (int i : rwx_regions) {
+                    comments += std::to_string(i / 1024)
+                        + "KB region with RWX rights detected!\n";
+                }
+            }
+            if (!rx_to_rw_regions.empty()) {
+                for (int i : rx_to_rw_regions) {
+                    comments += std::to_string(i / 1024)
+                        + "KB region with switched from RX to RW rights detected!\n";
+                }
+            }
+            if (!rw_to_rx_regions.empty()) {
+                for (int i : rw_to_rx_regions) {
+                    comments += std::to_string(i / 1024)
+                        + "KB region with switched from RW to RX rights detected!\n";
                 }
             }
             return comments;
