1990996ea5d6af4f7f1c74c1d70389bef2ea2f17
myanwc <anton.kostyrev@yandex.ru>
2026-02-11 21:32:09 +0300

    Add GUI for example of usage Application

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,31 @@
-﻿#include "application.h"
-#include "logger.h"
+﻿#include "logger.h"
+#include "gui.h"
 
-#include <iostream>
-#include <memory>
+#include <exception>
+#include <Windows.h>
 
-void ScanForHiddenProcesses(application::Application& application) {
-    auto hp = application.DetectHiddenProcesses();
-    if (hp.empty()) {
-        LOG_INFO("No hidden processes found");
-    }
-    else {
-        for (const auto& hidden_proc : hp) {
-            std::cout << "Suspicious process: \n"
-                << "[" << hidden_proc.pid_ << "] " 
-                <<hidden_proc.process_name_
-                << "\nReason: " << hidden_proc.comment_ << "\n";
-        }
-    }
-}
 
-void ScanForCompromisedProcesses(application::Application& application) {
-    auto cp = application.DetectCompromisedProcesses();
-    if (cp.empty()) {
-        LOG_INFO("No compromised processes found");
-    }
-    else {
-        for (const auto& compromised_proc : cp) {
-            std::cout << "Suspicious process: \n"
-                << "[" << compromised_proc.pid_ << "] "
-                << compromised_proc.process_name_
-                << "\nReason: " << compromised_proc.comment_ << "\n";
-        }
-    }
-}
+
+using namespace std::literals;
 
 int main() {
-    logging::Logger logger;
-    logger.Init();
-
     try {
-        application::Application application;
-        ScanForHiddenProcesses(application);
-        ScanForCompromisedProcesses(application);
+        logging::Logger logger;
+        logger.Init();
+
+        LPCWSTR wnd_name = L"ProcLab";
+        HINSTANCE hInstance = GetModuleHandleW(NULL);
+        GUI gui;
+        gui.Create(wnd_name, hInstance);
+        gui.Start();
     }
     catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        LOG_CRITICAL("System error: "s.append(e.what()));
+        MessageBox(NULL, e.what(), "System Error", MB_ICONERROR);
     }
 
-
 }
+
+
+
+
