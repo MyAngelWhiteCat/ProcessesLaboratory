7e061179c2b08c7844e5fea04577a642f7503fa6
myanwc <anton.kostyrev@yandex.ru>
2026-02-19 12:26:11 +0300

    init privilege analyzer

---
+++ src/Analyzers/EscalatedPrivileges/privilege_analyzer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../analyzer.h"
+
+namespace labaratory {
+
+    namespace analyze {
+
+        class PrivilegeAnalyzer : public Analyzer {
+
+        public:
+
+        private:
+
+
+        };
+
+    }
+
+}
