178679f3bbccf72d5516418aa957a67a335974cd
myanwc <anton.kostyrev@yandex.ru>
2026-02-28 21:15:27 +0300

    Add domain. Add process info class

---
+++ ProcessesLaboratoryGUI/Domain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessesLaboratoryGUI
+{
+    internal class ProcessInfo
+    {
+        public string process_info { get; set; }
+        public string pid { get; set; }
+        public string comment { get; set; }
+    }
+}
