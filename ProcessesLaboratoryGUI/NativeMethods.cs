75839e4b5b7dd379473b9bbc8d0b9f0270d07e50
myanwc <anton.kostyrev@yandex.ru>
2026-02-28 16:11:05 +0300

    Import dll methods

---
+++ ProcessesLaboratoryGUI/NativeMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ProcessesLaboratoryGUI
+{
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void LogCallback(string message);
+
+    internal static class NativeMethods
+    {
+
+        [DllImport("ProcessesLaboratoryApp.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DetectHiddenProcesses(LogCallback callback);
+
+        [DllImport("ProcessesLaboratoryApp.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DetectCompromisedProcesses(LogCallback callback);
+
+        [DllImport("ProcessesLaboratoryApp.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DetectEnabledPrivileges(LogCallback callback);
+
+    }
+
+}
