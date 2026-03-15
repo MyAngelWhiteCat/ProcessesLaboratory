3f359984a38fb696279b4963b19d89d6e5c01c1a
myanwc <anton.kostyrev@yandex.ru>
2026-03-15 21:02:41 +0500

    Rename it to detector. Add init class and namespaces

---
+++ src/Analyzers/AdminRights/admin_rights_detector.cpp
@@ -0,0 +1,11 @@
+#include "admin_rights_detector.h"
+
+namespace laboratory {
+
+    namespace analyze {
+
+
+
+    }
+
+}
---
+++ src/Analyzers/AdminRights/admin_rights_detector.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace laboratory {
+
+    namespace analyze {
+        
+        class AdminRightsDetector {
+
+        };
+
+    }
+
+}
