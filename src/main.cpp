119ed35a3890630b84a953ba6a6936061f886bc1
myanwc <anton.kostyrev@yandex.ru>
2026-03-15 19:38:00 +0500

    change test use case

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,40 @@
 ﻿#include "Logger/logger.h"
-#include "application.h"
 
 #include <exception>
 #include <Windows.h>
+#include <iostream>
 
 
 using namespace std::literals;
 
-void TestConsole() {
-    application::Application app;
-    Sleep(5000);
-    int i = 10;
-    while (i-- > 0) {
-        app.AsyncDetectEnabledPrivileges([](auto susps) {
-            if (susps.empty()) {
-                LOG_INFO("Empty");
-            }
-            });
-    }
-    while (true) {
+typedef void (*LogCallback)(const char* result);
 
-    }
+typedef void(*pDetectCompromisedProcesses)(
+    LogCallback
+    );
+
+void TestCallback(const char* text) {
+    std::cout << text << std::endl;
 }
 
 int main() {
     try {
         logging::Logger logger;
         logger.Init();
+
+        HMODULE module = LoadLibraryA("ProcessesLaboratoryDLL.dll");
+        auto* DetectCompromisedProcesses = 
+            reinterpret_cast<pDetectCompromisedProcesses>
+            (GetProcAddress(module, "DetectCompromisedProcesses"));
+        
+        DetectCompromisedProcesses(TestCallback);
+        while (true) {
+            Sleep(1000);
+        }
     }
     catch (const std::exception& e) {
-        LOG_CRITICAL("System error: "s.append(e.what()));
-        MessageBox(NULL, e.what(), "System Error", MB_ICONERROR);
+        LOG_CRITICAL("Error: "s.append(e.what()));
+        MessageBox(NULL, e.what(), "Error", MB_ICONERROR);
     }
 
 }
