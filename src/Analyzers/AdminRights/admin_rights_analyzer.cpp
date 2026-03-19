4a43ff9f81b19cff08c2facb5c0294d0e6d4a9dc
myanwc <anton.kostyrev@yandex.ru>
2026-03-19 17:24:38 +0500

    Добавил реализацию метода проверки конткретного процесса на наличие прав администратора

--- src/Analyzers/AdminRights/admin_rights_analyzer.cpp
+++ src/Analyzers/AdminRights/admin_rights_analyzer.cpp
@@ -14,7 +14,25 @@ namespace laboratory {
         }
 
         std::pair<domain::Severity, std::string> AdminRightsAnalyzer::AnalyzeProcess(DWORD pid) {
-            return std::pair<domain::Severity, std::string>();
+            domain::Severity severity = domain::Severity::INFO;
+            std::string comment;
+
+            domain::RaiiHandle hProcess(GetNtHandle(pid, PROCESS_QUERY_INFORMATION));
+            domain::RaiiHandle hToken(GetProcessToken(hProcess.Get(), TOKEN_QUERY));
+
+            bool integrity_level = CheckByIntegrityLevel(hToken.Get());
+            bool admin_group = CheckByAdminGroud(hToken.Get());
+            bool is_elevated = CheckByUAC(hToken.Get());
+
+            if (admin_group && !is_elevated) {
+                comment = "Process in admin group";
+            }
+            else if (integrity_level || is_elevated) {
+                comment = "Process run as administrator";
+                severity = domain::Severity::SUSPICIOUS;
+            }
+
+            return { severity, comment };
         }
 
         bool AdminRightsAnalyzer::CheckByIntegrityLevel(HANDLE hToken) {
